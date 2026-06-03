35c5182ce9c8ba34765fb051fc9d1464b75b13a8
Nguyễn Đức Huy <kewwihuy@gmail.com>
2026-06-03 18:15:57 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import { Button } from 'react-native/types_generated/index';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    // console.log('đụ má mày')
+    setCount((prev) => {
+      return prev + 1
+    })
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Bạn mấy tuổi rồi: {count} chủi</Text>
       <StatusBar style="auto" />
+      {/* <Button></Button> */}
+      <Button title='Bấm Vào Đây'
+        onPress={handleClick}
+      />
+      <Button title='Reset'
+        onPress={() => {
+          setCount(() => {
+            return 0
+          })
+        }}
+      />
     </View>
   );
 }
